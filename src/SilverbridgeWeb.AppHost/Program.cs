1c5602cfa2ab42f93f04c4b7e944923aaeec66a8
Fearghal McShane <fearghal@outlook.com>
2025-06-16 23:13:11 +0100

    Add Webapi and aspire projects

---
+++ src/SilverbridgeWeb.AppHost/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres");
+
+var silverbridgeDb = postgres.AddDatabase("silverbridgeDb");
+
+var api = builder.AddProject<Projects.SilverbridgeWeb_Api>("silverbridgeweb-api")
+    .WithReference(silverbridgeDb)
+    .WaitFor(silverbridgeDb)
+    .WithHttpHealthCheck("/health"); ;
+
+builder.AddProject<Projects.SilverbridgeWeb>("frontend")
+    .WithExternalHttpEndpoints()
+    .WithUrlForEndpoint("https", url => url.DisplayText = "Website (HTTPS)")
+    .WithUrlForEndpoint("http", url => url.DisplayText = "Website (HTTP)")
+    .WithHttpHealthCheck("/health")
+    .WithReference(api)
+    .WaitFor(api);
+
+builder.Build().Run();
