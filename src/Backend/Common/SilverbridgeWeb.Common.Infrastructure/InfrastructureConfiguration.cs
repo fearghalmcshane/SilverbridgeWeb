efe1ec3810f9b70182299c53cd068bda03b438a4
Fearghal McShane <fearghal@outlook.com>
2025-07-12 23:02:54 +0100

    Add redis container cache and test endpoint

--- src/Backend/Common/SilverbridgeWeb.Common.Infrastructure/InfrastructureConfiguration.cs
+++ src/Backend/Common/SilverbridgeWeb.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
+using SilverbridgeWeb.Common.Application.Caching;
 using SilverbridgeWeb.Common.Application.Clock;
 using SilverbridgeWeb.Common.Application.Data;
+using SilverbridgeWeb.Common.Infrastructure.Caching;
 using SilverbridgeWeb.Common.Infrastructure.Clock;
 using SilverbridgeWeb.Common.Infrastructure.Data;
+using StackExchange.Redis;
 
 namespace SilverbridgeWeb.Common.Infrastructure;
 
 public static class InfrastructureConfiguration
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString, string redisConnectionString)
     {
         NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
         services.TryAddSingleton(npgsqlDataSource);
@@ -18,6 +21,16 @@ public static class InfrastructureConfiguration
         services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
 
         services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+        services.TryAddSingleton(connectionMultiplexer);
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
+        });
+
+        services.TryAddSingleton<ICacheService, CacheService>();
 
         return services;
     }
