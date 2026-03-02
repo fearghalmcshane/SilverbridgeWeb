c14dda4195db633945510a794c9f931f3c300555
Fearghal McShane <31112608+fearghalmcshane@users.noreply.github.com>
2026-03-02 23:32:25 +0000

    Updated dependencies and upgraded xunit version (#138)
    Co-authored-by: Fearghal McShane <fearghal@outlook.com>

--- src/Backend/Modules/Events/test/SilverbridgeWeb.Modules.Events.ArchitectureTests/Abstractions/TestResultExtensions.cs
+++ src/Backend/Modules/Events/test/SilverbridgeWeb.Modules.Events.ArchitectureTests/Abstractions/TestResultExtensions.cs
@@ -1,11 +1,10 @@
 ﻿using FluentAssertions;
-using NetArchTest.Rules;
 
 namespace SilverbridgeWeb.Modules.Events.ArchitectureTests.Abstractions;
 
 internal static class TestResultExtensions
 {
-    internal static void ShouldBeSuccessful(this TestResult testResult)
+    internal static void ShouldBeSuccessful(this NetArchTest.Rules.TestResult testResult)
     {
         testResult.FailingTypes?.Should().BeEmpty();
     }
